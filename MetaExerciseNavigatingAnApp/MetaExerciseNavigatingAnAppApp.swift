21944895ef700b877a1c720a382eb40260aa4bc8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-29 17:19:31 -0600

    Initial Commit

---
+++ MetaExerciseNavigatingAnApp/MetaExerciseNavigatingAnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaExerciseNavigatingAnAppApp.swift
+//  MetaExerciseNavigatingAnApp
+//
+//  Created by Mitch Andrade on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaExerciseNavigatingAnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
